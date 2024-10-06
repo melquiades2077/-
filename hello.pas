e6144a270a7fab7c8ce11300d5f044afa1f9fddb
Vitaliy Afanasyev <vitafanasyev2000@yandex.ru>
2024-10-06 23:52:42 +0300

    Add 5 languages

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  
+  PrintLn($'Выберите свой язык:{newline}Русский: RU{newline}Английский: EN{newline}Испанский: SP{newline}Французский: FR{newline}Итальянский: IT');
+  var language := ReadString();
+  case language of
+    'RU': Print('Здравствуйте!');
+    'EN': Print('Hello!');
+    'SP': Print('Hola!');
+    'FR': Print('Bonjour!');
+    'IT': Print('Ciao!')
+    else Print('Данный язык не поддерживается.');
+  end;
 end.
